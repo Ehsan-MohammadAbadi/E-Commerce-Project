8f4abaa2697e9a353d25a9ead92e8a00185e1d6d
Ehsan MohammadAbadi <ehsan.mohmmadi2130@gmail.com>
2025-12-23 00:10:42 +0330

    Update index.blade.php
    show Dashboard button when logged in

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -3,14 +3,14 @@
 
 <head>
     <!-- Basic -->
-    <meta charset="utf-8" />
-    <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+    <meta charset="utf-8"/>
+    <meta http-equiv="X-UA-Compatible" content="IE=edge"/>
     <!-- Mobile Metas -->
-    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no"/>
     <!-- Site Metas -->
-    <meta name="keywords" content="" />
-    <meta name="description" content="" />
-    <meta name="author" content="" />
+    <meta name="keywords" content=""/>
+    <meta name="description" content=""/>
+    <meta name="author" content=""/>
     <link rel="shortcut icon" href="front/images/favicon.png" type="image/x-icon">
 
     <title>
@@ -18,15 +18,16 @@
     </title>
 
     <!-- slider stylesheet -->
-    <link rel="stylesheet" type="text/css" href="https://cdnjs.cloudflare.com/ajax/libs/OwlCarousel2/2.3.4/assets/owl.carousel.min.css" />
+    <link rel="stylesheet" type="text/css"
+          href="https://cdnjs.cloudflare.com/ajax/libs/OwlCarousel2/2.3.4/assets/owl.carousel.min.css"/>
 
     <!-- bootstrap core css -->
-    <link rel="stylesheet" type="text/css" href="front/css/bootstrap.css" />
+    <link rel="stylesheet" type="text/css" href="front/css/bootstrap.css"/>
 
     <!-- Custom styles for this template -->
-    <link href="front/css/style.css" rel="stylesheet" />
+    <link href="front/css/style.css" rel="stylesheet"/>
     <!-- responsive style -->
-    <link href="front/css/responsive.css" rel="stylesheet" />
+    <link href="front/css/responsive.css" rel="stylesheet"/>
 </head>
 
 <body>
@@ -39,7 +40,8 @@
             Giftos
           </span>
             </a>
-            <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+            <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent"
+                    aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
                 <span class=""></span>
             </button>
 
@@ -67,19 +69,25 @@
                         <a class="nav-link" href="front/contact.html">Contact Us</a>
                     </li>
                 </ul>
+
                 <div class="user_option">
-                    <a href="{{route('login')}}">
-                        <i class="fa fa-user" aria-hidden="true"></i>
-                        <span>
-                Login
-              </span>
-                    </a>
-                    <a href="{{route('register')}}">
-                        <i class="fa fa-user" aria-hidden="true"></i>
-                        <span>
-                Sign UP
-              </span>
-                    </a>
+                    @if(Auth::check())
+                        <a href="{{route('dashboard')}}">
+                            <i class="fa fa-user" aria-hidden="true"></i>
+                            <span>Dashboard</span>
+                        </a>
+
+                    @else
+                        <a href="{{route('login')}}">
+                            <i class="fa fa-user" aria-hidden="true"></i>
+                            <span>Login</span>
+                        </a>
+                        <a href="{{route('register')}}">
+                            <i class="fa fa-user" aria-hidden="true"></i>
+                            <span>Sign UP</span>
+                        </a>
+                    @endif
+
                     <a href="">
                         <i class="fa fa-shopping-bag" aria-hidden="true"></i>
                     </a>
@@ -109,7 +117,10 @@
                                             Gift Shop
                                         </h1>
                                         <p>
-                                            Sequi perspiciatis nulla reiciendis, rem, tenetur impedit, eveniet non necessitatibus error distinctio mollitia suscipit. Nostrum fugit doloribus consequatur distinctio esse, possimus maiores aliquid repellat beatae cum, perspiciatis enim, accusantium perferendis.
+                                            Sequi perspiciatis nulla reiciendis, rem, tenetur impedit, eveniet non
+                                            necessitatibus error distinctio mollitia suscipit. Nostrum fugit doloribus
+                                            consequatur distinctio esse, possimus maiores aliquid repellat beatae cum,
+                                            perspiciatis enim, accusantium perferendis.
                                         </p>
                                         <a href="">
                                             Contact Us
@@ -118,7 +129,7 @@
                                 </div>
                                 <div class="col-md-5 ">
                                     <div class="img-box">
-                                        <img style="width:600px" src="front/images/image3.jpeg" alt="" />
+                                        <img style="width:600px" src="front/images/image3.jpeg" alt=""/>
                                     </div>
                                 </div>
                             </div>
@@ -357,11 +368,6 @@
 <!-- end shop section -->
 
 
-
-
-
-
-
 <!-- contact section -->
 
 <section class="contact_section ">
@@ -377,23 +383,26 @@
             <div class="col-lg-7 col-md-6 px-0">
                 <div class="map_container">
                     <div class="map-responsive">
-                        <iframe src="https://www.google.com/maps/embed/v1/place?key=AIzaSyA0s1a7phLN0iaD6-UE7m4qP-z21pH0eSc&q=Eiffel+Tower+Paris+France" width="600" height="300" frameborder="0" style="border:0; width: 100%; height:100%" allowfullscreen></iframe>
+                        <iframe
+                            src="https://www.google.com/maps/embed/v1/place?key=AIzaSyA0s1a7phLN0iaD6-UE7m4qP-z21pH0eSc&q=Eiffel+Tower+Paris+France"
+                            width="600" height="300" frameborder="0" style="border:0; width: 100%; height:100%"
+                            allowfullscreen></iframe>
                     </div>
                 </div>
             </div>
             <div class="col-md-6 col-lg-5 px-0">
                 <form action="#">
                     <div>
-                        <input type="text" placeholder="Name" />
+                        <input type="text" placeholder="Name"/>
                     </div>
                     <div>
-                        <input type="email" placeholder="Email" />
+                        <input type="email" placeholder="Email"/>
                     </div>
                     <div>
-                        <input type="text" placeholder="Phone" />
+                        <input type="text" placeholder="Phone"/>
                     </div>
                     <div>
-                        <input type="text" class="message-box" placeholder="Message" />
+                        <input type="text" class="message-box" placeholder="Message"/>
                     </div>
                     <div class="d-flex ">
                         <button>
@@ -409,7 +418,6 @@
 <br><br><br>
 
 <!-- end contact section -->
-
 
 
 <!-- info section -->
@@ -439,7 +447,8 @@
                         ABOUT US
                     </h6>
                     <p>
-                        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed doLorem ipsum dolor sit amet, consectetur adipiscing elit, sed doLorem ipsum dolor sit amet,
+                        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed doLorem ipsum dolor sit amet,
+                        consectetur adipiscing elit, sed doLorem ipsum dolor sit amet,
                     </p>
                 </div>
                 <div class="col-md-6 col-lg-3">
@@ -460,7 +469,8 @@
                         NEED HELP
                     </h6>
                     <p>
-                        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed doLorem ipsum dolor sit amet, consectetur adipiscing elit, sed doLorem ipsum dolor sit amet,
+                        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed doLorem ipsum dolor sit amet,
+                        consectetur adipiscing elit, sed doLorem ipsum dolor sit amet,
                     </p>
                 </div>
                 <div class="col-md-6 col-lg-3">
