377456e7da7adfa11562759695acc8ccd791227c
Ehsan MohammadAbadi <ehsan.mohmmadi2130@gmail.com>
2025-12-22 21:21:37 +0330

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -4,12 +4,17 @@ use App\Http\Controllers\ProfileController;
 use App\Http\Controllers\UserController;
 use Illuminate\Support\Facades\Route;
 
+//Home page
 Route::get('/', function () {
     return view('welcome');
 });
 
+
+// Separate admin and user dashboard
 Route::get('/dashboard', [UserController::class, 'index'])->middleware(['auth', 'verified'])->name('dashboard');
 
+
+//Auth
 Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
